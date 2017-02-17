26b2fbf977355807ca89008b3f0467cb560761ac
Thomas Holme <thomas@tholme.net>
2017-02-17 22:14:40 +0100

    webcam-to-movie.sh: Fixed echoed message

--- webcam-to-movie.sh
+++ webcam-to-movie.sh
@@ -5,5 +5,5 @@ else
 do
   echo "Syntax:  webcam-to-movie.sh YEAR-MONTH NAME"
   echo "Example: webcam-to-movie.sh 2009-01 arsvaagen"
-  echo "         Will run: for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
+  echo "Will run: for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
 fi
