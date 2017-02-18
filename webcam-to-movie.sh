5a22eed07963efc5d54f528ac59db6fdd791c920
Thomas Holme <thomas@tholme.net>
2017-02-18 15:34:09 +0100

    webcam-to-movie.sh: Rearranged input arguments

--- webcam-to-movie.sh
+++ webcam-to-movie.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 if [ -z $2 ]
 then
-  echo "Syntax:  webcam-to-movie.sh YEAR-MONTH NAME"
-  echo "Example: webcam-to-movie.sh 2009-01 arsvaagen"
+  echo "Syntax:  webcam-to-movie.sh NAME YEAR-MONTH"
+  echo "Example: webcam-to-movie.sh arsvaagen 2009-01"
   echo "Will run: for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
   exit 1
 fi
 
-for i in $1-??/; do cat $i/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../$2-$1.mp4
+for i in $2-??/; do cat $i/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../$1-$2.mp4
 
