beb1bdbf390b31b7b2d95d096e1319e314524a77
Thomas Holme <thomas@tholme.net>
2017-02-20 20:56:30 +0100

    webcam-to-movie.sh: Allow more than one month to be added as argument. Allows creating more movie-files at once.

--- webcam-to-movie.sh
+++ webcam-to-movie.sh
@@ -2,10 +2,18 @@
 if [ -z $2 ]
 then
   echo "Syntax:  webcam-to-movie.sh NAME YEAR-MONTH"
-  echo "Example: webcam-to-movie.sh arsvaagen 2009-01"
-  echo "Will run: for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
+  echo "Example: webcam-to-movie.sh arsvaagen 2009-01 2009-02 2009-03"
+  echo "Will run:"
+  echo "for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
+  echo "for i in 2009-02-??/; do cat 2009-02/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-02.mp4"
+  echo "for i in 2009-03-??/; do cat 2009-03/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-03.mp4"
   exit 1
 fi
 
-for i in $2-??/; do cat $i/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../$1-$2.mp4
+#Get all arguments starting from argument 2
+for MONTHS in ${@:2}
+do
+  #Pipe all jpeg files for one month to ffmpeg and output as mp4 file. Quality: crf 28
+  for i in $MONTHS-??/; do cat $i/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../$1-$MONTHS.mp4
+done
 
