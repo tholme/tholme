d00f2e97c1fd0a442918c0d10293f1e31568af8f
Thomas Holme <thomas@tholme.net>
2017-02-20 21:19:43 +0100

    Fix help text and add example in help text

--- webcam-to-movie.sh
+++ webcam-to-movie.sh
@@ -1,12 +1,16 @@
 #!/bin/sh
 if [ -z $2 ]
 then
-  echo "Syntax:  webcam-to-movie.sh NAME YEAR-MONTH"
+  echo "Syntax:  webcam-to-movie.sh NAME YEAR-MONTH [YEAR-MONTH ...]"
   echo "Example: webcam-to-movie.sh arsvaagen 2009-01 2009-02 2009-03"
   echo "Will run:"
   echo "for i in 2009-01-??/; do cat 2009-01/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-01.mp4"
   echo "for i in 2009-02-??/; do cat 2009-02/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-02.mp4"
   echo "for i in 2009-03-??/; do cat 2009-03/*.jpg ;done | ffmpeg -f image2pipe -i - -crf 28 ../arsvaagen-2009-03.mp4"
+  echo ""
+  echo "Example for whole year:"
+  echo "webcam-to-movie.sh arsvaagen 2011-{01..12}"
+  echo ""
   exit 1
 fi
 
