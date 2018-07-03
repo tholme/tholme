25fc634d4389978b91e58cc95643f733ad875f77
Thomas Holme <thomas@tholme.net>
2018-07-03 14:10:01 +0200

    download-webcam-stream: Use "/media/2tb/webcam/$DIRNAVN/stream/url" to get URL instead of $2. This allows using instantiated systemd service with only url config file.

--- download-webcam-stream.sh
+++ download-webcam-stream.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
-if [ -z "$2" ]
+if [ -z "$1" ]
 then
-  echo "Usage: `basename $0` DIRNAVN WEBCAMURL"
+  echo "Usage: `basename $0` DIRNAVN"
   exit $E_NOARGS
 fi
 
@@ -10,19 +10,26 @@ fi
 #DIRNAVN=`basename $1 .jpg`
 
 DIRNAVN=$1
-URL="$2"
 
-#Trao ctrl+x
+if [ ! -f "/media/2tb/webcam/$DIRNAVN/stream/url" ]; then
+	echo "/media/2tb/webcam/$DIRNAVN/stream/url does not exist"
+	exit 3
+fi
+
+read URL < /media/2tb/webcam/$DIRNAVN/stream/url
+
+#Trap ctrl+c
 trap trapint 2
 function trapint {
     exit 0
 }
 
-mkdir -p /home/thomas/webcam/$DIRNAVN/stream
-cd /home/thomas/webcam/$DIRNAVN
+cd /media/2tb/webcam/$DIRNAVN
 
 while true
 do
-  ffmpeg -loglevel warning -i "$2" -c:v copy  -c:a copy stream/$DIRNAVN-`date +%F-%T`.ts
-  sleep 10
+  MONTH=`date +%Y-%m`
+  mkdir -p stream/$MONTH
+  ffmpeg -loglevel warning -i "$URL" -vcodec copy -c:v copy  -c:a copy stream/$MONTH/$DIRNAVN-`date +%F-%T`.ts
+  sleep 60
 done
