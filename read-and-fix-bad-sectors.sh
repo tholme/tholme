92f1bf0349ebc2997856085347a57e73de8d38ec
Thomas Holme <thomas@tholme.net>
2018-07-03 14:12:28 +0200

    read-and-fix-bad-sectors.sh: Print out sector to screen.

--- read-and-fix-bad-sectors.sh
+++ read-and-fix-bad-sectors.sh
@@ -1,5 +1,6 @@
 for i in $(cat $1); do
   for ((j=-200; j<=200; j++)) ; do
+    echo $(($i+$j))
     sudo hdparm -q --read-sector $(($i+$j)) /dev/sda >/dev/null || sudo hdparm --yes-i-know-what-i-am-doing --write-sector $(($i+$j)) /dev/sda
  done
 done
