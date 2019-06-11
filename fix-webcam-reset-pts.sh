dc302839f16d81e9fa625a53bfd418dc8d31fe88
Thomas Holme <thomas@tholme.net>
2019-06-11 17:37:39 +0200

    Added fix-webcam-reset-pth.sh. Converts from ts to mp4 and speeds up file.

---
+++ fix-webcam-reset-pts.sh
@@ -0,0 +1,21 @@
+#FILENAME=$1
+#OUTNAME=${FILENAME%.*}.mp4
+
+#Delete ts files smaller than 160k
+find . -type f -name "*.ts" -maxdepth 1 -size -360k -delete 
+
+#Convert to mp4
+
+find . -type f -name '*.ts' -maxdepth 1 -exec sh -c '
+  for file do
+    OUTNAME=${file%.*}.mp4
+    echo "$file -> $OUTNAME" 
+    #Resets pts and set framerate to 60fps to speed up movie
+    ffmpeg -hide_banner -loglevel error -i "file:$file" -c copy -f h264 - | ffmpeg -hide_banner -loglevel error -r 60 -i pipe: -vcodec copy "file:$OUTNAME"
+    if [ -s "$file" ]
+    then
+      rm $file
+    fi
+  done
+' sh {} +
+
